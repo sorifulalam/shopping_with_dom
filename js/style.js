84a7f2d4bbddb2ed9ac7d8bdc41107fac9b604ed
Soriful Alam <58595744+sorifulalam@users.noreply.github.com>
2023-02-21 00:41:42 +0600

    js code adding card

---
+++ js/style.js
@@ -0,0 +1,7 @@
+document.getElementById("first-card").addEventListener("click", function(){
+    const firsttitel = document.getElementById("first-name").innerText;
+    const firstPrice = document.getElementById("first-price").innerText;
+    const firstQuentity =document.getElementById("first-quantity").innerText;
+
+    console.log(firstPrice,firstQuentity);
+})
